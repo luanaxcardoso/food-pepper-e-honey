f294ee8c03904d2e7340b2a9599e154ff8a49152
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-06-24 12:58:50 -0300

    feat: buttons

--- src/javascript/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('#mobile_btn').on('click', function() {
-        $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x');
-    });
-});
