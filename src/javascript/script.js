8fabf9dec55a774e4f723cc901f238d9438c8681
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-06-26 20:00:14 -0300

    feat: particles

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -5,12 +5,20 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
+    $('.btn-default-cart').on('click', function() {
+        alert('Carrinho em construção! Em breve você poderá finalizar sua compra!');
+    });
+});
+
+
+
+$(document).ready(function() {
     
     $(".hidden").hide();
     
     $(".btn-default-test").click(function() {
         $(".hidden").slideToggle();
-        
+  
         
         $(this).text(function(i, text){
             return text === "Ver mais avaliações" ? "Ver menos avaliações" : "Ver mais avaliações";
@@ -18,19 +26,116 @@ $(document).ready(function() {
     });
 });
 
-$(document).ready(function() {
-    
-    var $img = $('#banner img');
-    
-    
-    $img.hide();
-    
-    
-    $img.fadeIn(1000); 
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ff6"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 15
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ff6600",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
 });
 
-$(document).ready(function() {
-    $('.btn-default-cart').click(function() {
-        alert('O carrinho está em construção. Em breve estará disponível!');
-    });
-});
+
+
